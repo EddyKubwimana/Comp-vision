401cee141ff47fdaf9df659bba2a28719610492a
EddyKubwimana <kubwimanaeddy1@gmail.com>
2022-01-26 20:01:41 +0000

    drop a box added

--- main.py
+++ main.py
@@ -9,25 +9,31 @@ detector=HandDetector(detectionCon=0.8,maxHands=2)
 color = (0, 255, 0)
 x = 100
 y = 200
-w = 300
-h = 400
+w = 150
+h = 250
 while True:
 
     success,img=cap.read()
-    cv2.rectangle(img,(x,y),(w,h),color,cv2.FILLED)
+
     Hands,img= detector.findHands(img)
+    class mytryiangle():
+        def __init__(self,x,y,w,h):
+            self.x=x
+            self.y=y
+            self.w=w
+            self.h=h
+
     if Hands:
         Hands1=Hands[0]
         lmlist=Hands1['lmList']
         cursor=lmlist[8]
         lenght,info,img=detector.findDistance(lmlist[8],lmlist[12],img)
-        if x<cursor[0]<w and y<cursor[1]<h and lenght<30:
+        if x-w//2 <cursor[0]< x+w//2 and y-h//2 <cursor[1]<y+h//2 and lenght<=30:
                 color = (0, 255, 255)
-                x=cursor[0]/2
-                y=cursor[1]/2
-                w=x+200
-                h=y+200
-
+                x,y=cursor
+        else:
+            color=(0, 255, 0)
+    cv2.rectangle(img, (x-w//2, y-h//2), (x+w//2, y+h//2), color, cv2.FILLED)
 
     cv2.imshow("image",img)
     cv2.waitKey(1)
